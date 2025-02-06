4c4fe470c56245d08540f27f4f950feeb53296f0
Rlohaw <rl0h4w@gmail.com>
2025-02-06 16:46:31 +0300

    first commit

---
+++ Java/1/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+class Goodbye {
+    public static void main(String[] args) {
+        System.out.println("Bye, World!");
+    }
+}
+
+class Something {
+    public static void test(String[] args) {
+        System.out.println("bebebebe");
+    }
+}
