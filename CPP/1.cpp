fe55dc762f43949b42bef068980c68fca92e231b
rl0h4w <rl0h4w@gmail.com>
2025-03-08 12:10:04 +0300

    half of discrete

--- CPP/1.cpp
+++ CPP/1.cpp
@@ -1,19 +1,55 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res = {-1, -1}; 
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < nums.size(); ++j) {
-                if (i != j) { /
-                    int sum = nums[i] + nums[j];
-                    if (sum == target) {
-                        res[0] = i;
-                        res[1] = j;
-                        return res; 
-                    }
-                }
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int countValidCombinations(int n, int k, int min_gap = 3)
+{
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i)
+    {
+        nums[i] = i + 1;
+    }
+
+    vector<bool> select(n, false);
+    fill(select.end() - k, select.end(), true);
+
+    int count = 0;
+    do
+    {
+        vector<int> combo;
+        for (int i = 0; i < n; ++i)
+        {
+            if (select[i])
+            {
+                combo.push_back(nums[i]);
             }
         }
-        return res;
-    }
-};
+
+        bool valid = true;
+        for (size_t i = 1; i < combo.size(); ++i)
+        {
+            if (combo[i] - combo[i - 1] <= min_gap)
+            {
+                valid = false;
+                break;
+            }
+        }
+
+        if (valid)
+        {
+            count++;
+            cout << count << endl;
+        }
+
+    } while (next_permutation(select.begin(), select.end()));
+
+    return count;
+}
+
+int main()
+{
+    cout << countValidCombinations(50, 10) << endl;
+    return 0;
+}
